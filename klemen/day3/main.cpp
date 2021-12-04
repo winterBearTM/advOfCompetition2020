33cdcb08798c36866749288a4ad510b05f03136d
klemenkolenc <klemen.kalinc@gmail.com>
2021-12-04 09:25:41 +0100

    add clean template for day3

---
+++ klemen/day3/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <chrono>
+
+using namespace std;
+
+int main(){
+   auto start = chrono::high_resolution_clock::now();
+   cout << "starting..." << "\n";
+
+   fstream newfile;
+   newfile.open("input.txt",ios::in); //open a file to perform read operation using file object
+   if (newfile.is_open()){   //checking whether the file is open
+      // Declarations
+
+      // Operations
+
+      
+      newfile.close(); //close the file object.
+   }else{
+      cout << "file was not opened." << "\n";
+   }
+
+   auto stop = chrono::high_resolution_clock::now();
+   auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+   cout << "time elapsed: "<< duration.count() << "us" << endl;
+}
