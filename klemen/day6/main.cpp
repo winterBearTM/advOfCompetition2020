fa12d807d371f9b9cdf98326441e468998c57dc8
klemenkolenc <klemen.kalinc@gmail.com>
2021-12-07 21:12:04 +0100

    add empty day6

---
+++ klemen/day6/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <chrono>
+
+using namespace std;
+
+
+
+int main(){
+   auto start = chrono::high_resolution_clock::now();
+   cout << "starting..." << "\n";
+
+   fstream newfile;
+   newfile.open("input.txt",ios::in); //open a file to perform read operation using file object
+   if (newfile.is_open()){   //checking whether the file is open
+
+
+      // Declarations
+      vector<string> lines;
+      string row;
+
+  
+      // Load input into memory
+      while(getline(newfile, row)){ //read data from file object and put it into string.
+         lines.push_back(row);
+      }
+
+      // Operations
+
+      newfile.close(); //close the file object.
+   }else{
+      cout << "file was not opened." << "\n";
+   }
+
+   auto stop = chrono::high_resolution_clock::now();
+   auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+   cout << "time elapsed: "<< duration.count() << "us" << endl;
+}
